153b2d1b2974a663bcdf186e5f7fdd3ee073c73e
b3tch1 <167640503+b3tch1@users.noreply.github.com>
2024-10-03 19:33:38 -0500

    Initial Commit

--- SudokuBox.java
+++ SudokuBox.java
@@ -1,3 +1,46 @@
+import java.util.HashSet;
+
 public class SudokuBox {
-    
+    int row, column;
+    HashSet<Integer> possibleValues;
+
+    public SudokuBox(int i, int j){
+        this.row = i;
+        this.column = j;
+        this.possibleValues = new HashSet<>();
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getColumn(){
+        return column;
+    }
+
+    public int numPossibleValues(){
+        return possibleValues.size();
+    }
+
+    public int[] getPossibleValues(){
+        int[] values = new int[possibleValues.size()];
+        int count = 0;
+        for(int i : possibleValues){
+            values[count] = i;
+            count++;
+        }
+        return values;
+    }
+
+    public void chooseValue(int val){
+
+    }
+
+    private void updatePossibleValues(){
+        
+    }
+
+    private void initializePossibleValues(){
+
+    }
 }
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,4 +1,47 @@
-
 class SudokuSolver{
 
+    private static boolean verifyInitialBoard(int[][] board){
+        return false;
+    }
+
+    private static SudokuBox[][] initializeSudokuBoard(int[][] board){
+        SudokuBox[][] sudokuBoard = new SudokuBox[9][9];
+
+        return sudokuBoard;
+    }
+
+    private static SudokuBox getBestBox(SudokuBox[][] board){
+        SudokuBox bestBox = board[0][0];
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(board[i][j].numPossibleValues() > 1 && board[i][j].numPossibleValues() < bestBox.numPossibleValues()){
+                    bestBox = board[i][j];
+                }
+            }
+        }
+        return bestBox;
+    }
+
+    private static void tryValues(SudokuBox[][] board){
+        SudokuBox box = getBestBox(board);
+        int[] values = box.getPossibleValues();
+        for(int val : values){
+            board[box.getRow()][box.getColumn()].chooseValue(val);
+        }
+    }
+
+    public static void main(String args[]){
+        int[][] initialBoard = new int[9][9];
+
+        if(!verifyInitialBoard(initialBoard)){
+            System.out.println("Invalid Input");
+            return;
+        }
+            
+        SudokuBox[][] sudokuBoard = initializeSudokuBoard(initialBoard);
+        while(true){
+            tryValues(sudokuBoard);
+        }
+        
+    }
 }
